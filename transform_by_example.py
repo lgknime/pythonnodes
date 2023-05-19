1869e490f3668fa273dd1a216476c12238d5a777
Linus Krause <Linus.Krause@knime308.kn.knime.com>
2023-05-19 18:28:31 +0200

    working at webscraping with knime

--- transform_by_example.py
+++ transform_by_example.py
@@ -3,8 +3,21 @@ import math
 import pandas as pd
 import numpy as np
 import random
+import requests as re
+from bs4 import BeautifulSoup
+import os
 
 
 
-B = random.choice([1,2,3,4])
-print(B)
+knime_data = pd.read_csv("data/missingdata.csv")
+obj = knime_data.loc[0, "Company"].lower().replace(" ", "_")
+
+
+
+
+x = re.get(f'https://wikipedia.org/wiki/KNIME').content
+
+soup = BeautifulSoup(x, 'html.parser')
+
+
+print(soup.find("table").find_all("td"))
