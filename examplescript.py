f0c1da07377433364d47818238ebd1550fb46ce7
Linus Krause <linus.krause@knime.com>
2025-12-01 16:49:57 +0100

    function

--- examplescript.py
+++ examplescript.py
@@ -1,3 +1,4 @@
-
-# Fill missing values in 'Currency Unit' column with "Dollar"
-df['Currency Unit'] = df['Currency Unit'].fillna("Dollar")
+def currencyfill(df):
+    # Fill missing values in 'Currency Unit' column with "Dollar"
+    df['Currency Unit'] = df['Currency Unit'].fillna("Dollar")
+    return df
